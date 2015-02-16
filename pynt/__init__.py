bef6d1d67b32db3edbcaf0b26b03606755915fed
Raghunandan Ramakrishna Rao <raghunr@amazon.com>
2015-02-16 13:09:44 +0530

    attributes for setup.py

--- pynt/__init__.py
+++ pynt/__init__.py
@@ -2,6 +2,3 @@
 Lightweight Python Build Tool
 """
 
-__version__ = "0.8.0"
-__license__ = "MIT License"
-__contact__ = "http://rags.github.com/pynt/"
---
+++ pynt/contrib/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.1.0"
+__license__ = "MIT License"
+__contact__ = "http://rags.github.com/pynt-contrib/"
